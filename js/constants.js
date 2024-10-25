bbae8b4b72714bd9bf57c725421c44eef51a0675
dinarcoins <nhanhoang863@gmail.com>
2024-10-25 23:53:15 +0700

    init code

---
+++ js/constants.js
@@ -0,0 +1,7 @@
+export var menuList = [
+  { title: "trang chủ", href: "#home" },
+  { title: "sơ lược", href: "#precis" },
+  { title: "lịch sử", href: "#history" },
+  { title: "bản quyền", href: "#banquyen" },
+  { title: "liên hệ", href: "#contact" },
+];
